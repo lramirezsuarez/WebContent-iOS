140aeae9a14ed55684883e12fb3ec3734a80b598
Luis Alejandro Ramirez <lramirezsuarez@gmail.com>
2017-06-06 16:12:24 -0500

    Web view usage with cimebi.com, HTML code with web view, and task requests.

--- WebContent/WebContent/ViewController.swift
+++ WebContent/WebContent/ViewController.swift
@@ -13,14 +13,40 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        if let url = URL(string: "http://www.cimebi.com") {
+        
+//        urlWebView.loadRequest(URLRequest(url: url!))
+//        
+//        let html = "<h1>Form inside the app</h1><br><form method='post' action='#'><input type='text' name='name' placeholder='Escriba su nombre' /><br><input type='submit' name='Enviar' /></form>"
+//        
+//        urlWebView.loadHTMLString(html, baseURL: nil)
+//        
+        
+            let request = NSMutableURLRequest(url: url)
+            
+            let task = URLSession.shared.dataTask(with: request as URLRequest) {
+                data, response, error in
+                if(error != nil) {
+                    print("There was an error.")
+                } else {
+                    if let recievedData = data {
+                        let dataString = NSString(data: recievedData, encoding: String.Encoding.utf8.rawValue)
+                        
+                        print(dataString)
+                    }
+                }
+                
+            }
+            
+            task.resume()
+            
+            
+            
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
-
+    
 }
 
