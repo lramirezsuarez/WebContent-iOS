98c60a621a7288a4ac4e9a9aee0e86c81d47abb2
Luis Alejandro Ramirez <lramirezsuarez@gmail.com>
2018-02-04 22:26:14 -0500

    Uncommented lines and a file.

--- WebContent/WebContent/ViewController.swift
+++ WebContent/WebContent/ViewController.swift
@@ -16,30 +16,30 @@ class ViewController: UIViewController {
         
         if let url = URL(string: "http://www.cimebi.com") {
         
-        urlWebView.loadRequest(URLRequest(url: url))
+//        urlWebView.loadRequest(URLRequest(url: url))
         
 //        let html = "<h1>Form inside the app</h1><br><form method='post' action='#'><input type='text' name='name' placeholder='Escriba su nombre' /><br><input type='submit' name='Enviar' /></form>"
 //        
 //        urlWebView.loadHTMLString(html, baseURL: nil)
         
         
-//            let request = NSMutableURLRequest(url: url)
-//            
-//            let task = URLSession.shared.dataTask(with: request as URLRequest) {
-//                data, response, error in
-//                if(error != nil) {
-//                    print("There was an error.")
-//                } else {
-//                    if let recievedData = data {
-//                        let dataString = NSString(data: recievedData, encoding: String.Encoding.utf8.rawValue)
-//                        
-//                        print(dataString)
-//                    }
-//                }
-//                
-//            }
-//            
-//            task.resume()
+            let request = NSMutableURLRequest(url: url)
+            
+            let task = URLSession.shared.dataTask(with: request as URLRequest) {
+                data, response, error in
+                if(error != nil) {
+                    print("There was an error.")
+                } else {
+                    if let recievedData = data {
+                        let dataString = NSString(data: recievedData, encoding: String.Encoding.utf8.rawValue)
+                        
+                        print(dataString!)
+                    }
+                }
+                
+            }
+            
+            task.resume()
             
           }
     }
